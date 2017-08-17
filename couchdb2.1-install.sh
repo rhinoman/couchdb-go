6f9c79fa009735a9adea059b6bd004d2a5c9f808
Jace Ferguson <jace_ferguson@isdschools.org>
2017-08-17 14:43:09 -0500

    Major update to the install script for latest couch version.

--- couchdb2.1-install.sh
+++ couchdb2.1-install.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-echo "deb https://apache.bintray.com/couchdb-deb trusty main" \
-| sudo tee -a /etc/apt/sources.list
+set -ex
 
-curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc \
-    | sudo apt-key add -
+sudo apt-get --no-install-recommends -y install \
+    build-essential pkg-config erlang \
+    libicu-dev libmozjs185-dev libcurl4-openssl-dev
 
-sudo apt-get update || true
-sudo apt-get --no-install-recommends -y install couchdb
+mkdir temp
+cd temp
+
+wget http://www.trieuvan.com/apache/couchdb/source/2.1.0/apache-couchdb-2.1.0.tar.gz
+
+tar -xf apache-couchdb-2.1.0.tar.gz
+cd apache-couchdb-2.1.0.tar.gz
+./configure
+make release
+nohup ./rel/couchdb/bin/couchdb > /dev/null &
+
+curl -X PUT http://127.0.0.1:5984/_users
+
+curl -X PUT http://127.0.0.1:5984/_replicator
+
+
